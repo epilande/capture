337cfee10c9e758c61b40051a0dcf53e330d8d04
Emmanuel Pilande <epilande@gmail.com>
2016-01-30 18:29:53 -1000

    Use Input component from homepage

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Home from '../components/Home';
+import Input from '../components/Input';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <Home />
+      <Input onChange={this._onInputChange} />
     );
   }
 }
