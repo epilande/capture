be1dec5979365aa4c409b9b91f4687c5611f9e38
Emmanuel Pilande <epilande@gmail.com>
2016-01-31 23:30:26 -1000

    Fix routes

--- app/routes.js
+++ app/routes.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from 'containers/App';
-import HomePage from 'containers/HomePage';
+import Home from 'containers/Home';
 
 export default (
   <Route path="/" component={App}>
-    <IndexRoute component={HomePage} />
+    <IndexRoute component={Home} />
   </Route>
 );
