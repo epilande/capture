2b4af9a4f0a998cb773333c32345996d56039aa4
Emmanuel Pilande <epilande@gmail.com>
2016-02-03 22:49:18 -1000

    Better URL validation regex

--- app/utils/validation.js
+++ app/utils/validation.js
@@ -4,13 +4,10 @@
  * @returns {boolean} Url is valid or not
  */
 export function validUrl(url) {
-  const patterns = {
-    protocol: '^(http(s)?(:\/\/))?(www\.)?',
-    domain: '[a-zA-Z0-9-_\.]+',
-    tld: '(\.[a-zA-Z0-9]{2,})',
-    params: '([-a-zA-Z0-9:%_\+.~#?&//=]*)'
-  };
-  const { protocol, domain, tld, params } = patterns;
+  const protocol = '^((http(s)?(:\\/\\/))?(www\\.)?\\b)?';
+  const domain = '[-a-zA-Z0-9@:%._\\+~#=]{2,256}';
+  const tld = '((\\.|\\:)([a-z]{2,6}|\\d))';
+  const params = '(\\/[\\/\\d\\w\\.-]*)*([-a-zA-Z0-9:%()_\\+.~#?&//=]*)$';
   const pattern = new RegExp(protocol + domain + tld + params, 'i');
 
   return pattern.test(url);
