e4460bd9089e06d6a34e449c9bda97e6bbfb0f2b
Emmanuel Pilande <epilande@gmail.com>
2016-02-05 13:09:29 -1000

    Add youtube-dl & create downloadVideo function

---
+++ app/api/download.js
@@ -0,0 +1,27 @@
+/* eslint strict: 0, no-console: 0 */
+// import fs from 'fs';
+import ytdl from 'youtube-dl';
+
+export function downloadVideo() {
+  const video = ytdl('https://www.youtube.com/watch?v=YW1VS9Ftqdc', ['--format=18'], { cwd: __dirname });
+
+  video.on('error', (err) => {
+    console.log(err);
+  });
+
+  // ytdl.getInfo('https://www.google.com', ['--format=18'], (err, info) => {
+  //   console.log(err);
+  //   if (err) throw err;
+
+  //   console.log(info);
+  // });
+
+  // Will be called when the download starts.
+  video.on('info', (info) => {
+    console.log('Download started');
+    console.log(`filename: ${info._filename}`);
+    console.log('Info: ', info);
+  });
+
+  // video.pipe(fs.createWriteStream('myvideo.mp4'));
+}
