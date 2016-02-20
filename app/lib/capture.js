e3f3bfc0d6971391bd43b584edb886bc08e3744d
Emmanuel Pilande <epilande@gmail.com>
2016-02-20 10:47:00 -1000

    Refactor capture to be more modular

--- app/lib/capture.js
+++ app/lib/capture.js
@@ -3,25 +3,34 @@ import fs from 'fs';
 import path from 'path';
 import ytdl from 'youtube-dl';
 
-export function downloadVideo(url, options = [], output) {
-  console.log('downloadVideo func: ', url, options, output);
-  return new Promise((resolve, reject) => {
-    ytdl.getInfo(url, options, (err, info) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(info);
-    });
-  }).then((result) => {
-    console.log('Then result: ', result);
-    const video = ytdl(url, options, { cwd: __dirname });
+export function init(url, options = [], callback) {
+  const video = ytdl(url, options, { cwd: __dirname });
+  console.log('downloadVideo func: ', url, options);
 
-    video.on('error', (err) => {
-      console.log(err);
-    });
+  video.on('error', (err) => {
+    console.log(err);
+  });
 
-    video.pipe(fs.createWriteStream(path.resolve(output, `${result.title}.mp4`)));
-  }).catch((error) => {
-    console.log('Catch: ', error);
+  return callback(video);
+}
+
+export function download(video, output, callback) {
+  video.on('info', (info) => {
+    const file = path.resolve(output, `${info.title}.mp4`);
+    video.pipe(fs.createWriteStream(file));
+
+    return callback(info);
+  });
+}
+
+export function progress(video, size, callback) {
+  let pos = 0;
+  video.on('data', (chunk) => {
+    pos += chunk.length;
+    if (size) {
+      const percent = (pos / size * 100).toFixed(2);
+
+      return callback(percent);
+    }
   });
 }
