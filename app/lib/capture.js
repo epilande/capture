a6c06cedd7f83d35587e7ad9ca2b390f9f8a482c
Emmanuel Pilande <epilande@gmail.com>
2016-02-19 23:55:40 -1000

    Get video info before download & set video title as filename

--- app/lib/capture.js
+++ app/lib/capture.js
@@ -1,28 +1,28 @@
 /* eslint strict: 0, no-console: 0 */
-// import fs from 'fs';
-// import path from 'path';
+import fs from 'fs';
+import path from 'path';
 import ytdl from 'youtube-dl';
 
 export function downloadVideo(url, options = [], output) {
-  const video = ytdl(url, options, { cwd: __dirname });
+  ytdl.getInfo(url, options, (err, info) => {
+    console.log('downloadVideo func: ', url, options, output);
+    console.log('getInfo err: ', err);
+    if (err) throw err;
 
-  video.on('error', (err) => {
-    console.log(err);
+    const video = ytdl(url, options, { cwd: __dirname });
+
+    // video.on('error', (err) => {
+    //   console.log(err);
+    // });
+
+    console.log('getInfo info: ', info);
+
+    // video.on('info', (info) => {
+    //   console.log('Download started');
+    //   console.log(`filename: ${info._filename}`);
+    //   console.log('Info: ', info);
+    // });
+
+    video.pipe(fs.createWriteStream(path.resolve(output, `${info.title}.mp4`)));
   });
-
-  // ytdl.getInfo('https://www.google.com', ['--format=18'], (err, info) => {
-  //   console.log(err);
-  //   if (err) throw err;
-
-  //   console.log(info);
-  // });
-
-  // Will be called when the download starts.
-  video.on('info', (info) => {
-    console.log('Download started');
-    console.log(`filename: ${info._filename}`);
-    console.log('Info: ', info);
-  });
-
-  // video.pipe(fs.createWriteStream(path.resolve(output, 'myvideo.mp4')));
 }
