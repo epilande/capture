58c80b8af621ea84083bb34323c2f0971639c82c
Emmanuel Pilande <epilande@gmail.com>
2016-03-13 19:32:39 -1000

    Fix webpack prod config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -19,15 +19,13 @@ config.module.loaders.push({
   test: /\.global\.css$/,
   loader: ExtractTextPlugin.extract(
     'style-loader',
-    'css-loader',
-    'postcss-loader'
+    'css-loader!postcss-loader'
   ),
 }, {
   test: /^((?!\.global).)*\.css$/,
   loader: ExtractTextPlugin.extract(
     'style-loader',
-    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
-    'postcss-loader'
+    'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader', // eslint-disable-line
   ),
 });
 
