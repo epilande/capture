8776865d1cd74bd4fb4bb288d9dd540be4917455
Emmanuel Pilande <epilande@gmail.com>
2016-03-16 15:10:20 -1000

    Display item percent in List component

--- app/components/List/List.js
+++ app/components/List/List.js
@@ -2,7 +2,9 @@ import React, { PropTypes } from 'react';
 import styles from './List.css';
 
 const List = ({ items }) => {
-  const ListItems = items.map((item) => <li className={styles.item} key={item.id}>{item.url}</li>);
+  const ListItems = items.map((item) => (
+    <li className={styles.item} key={item.id}>{item.url} {item.percent}</li>
+  ));
 
   return (
     <ul className={styles.list}>
