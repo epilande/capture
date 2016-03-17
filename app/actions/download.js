ff0cac49df775647a66000b616e6c1c6df70c3f8
Emmanuel Pilande <epilande@gmail.com>
2016-03-16 15:09:05 -1000

    Generate Id before download

--- app/actions/download.js
+++ app/actions/download.js
@@ -4,27 +4,31 @@ export const ADD_ITEM = 'ADD_ITEM';
 export const PROGRESS_ITEM = 'PROGRESS_ITEM';
 
 export function download(url, options, output) {
-  return (dispatch) => {
-    console.log('download: ', url, options, output);
-    dispatch(addItem(url));
+  return (dispatch, getState) => {
+    const downloadState = getState().download;
+    const downloadId = downloadState.items.reduce((maxId, item) =>
+                                                  Math.max(item.id, maxId), -1) + 1;
+    console.log('download: ', url, options, output, downloadId);
+    dispatch(addItem(downloadId, url));
     capture.init(url, options, (video) => {
       capture.download(video, output, (info) => {
         console.log(info);
         capture.progress(video, info.size, (percent) => {
-          dispatch(progress(percent));
+          dispatch(progress(downloadId, percent));
         });
       });
     });
   };
 }
 
-export function addItem(url) {
-  return { type: ADD_ITEM, url };
+export function addItem(id, url) {
+  return { type: ADD_ITEM, id, url };
 }
 
-export function progress(percent, debounce = 50) {
+export function progress(id, percent, debounce = 50) {
   return {
     type: PROGRESS_ITEM,
+    id,
     percent,
     meta: { debounce: { time: debounce } },
   };
