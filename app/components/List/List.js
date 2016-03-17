6562ac37791544b06cd4ae92c06fe82bd99c6fa8
Emmanuel Pilande <epilande@gmail.com>
2016-03-16 19:14:10 -1000

    Display video title rather than url

--- app/components/List/List.js
+++ app/components/List/List.js
@@ -2,9 +2,15 @@ import React, { PropTypes } from 'react';
 import styles from './List.css';
 
 const List = ({ items }) => {
-  const ListItems = items.map((item) => (
-    <li className={styles.item} key={item.id}>{item.url} {item.percent}</li>
-  ));
+  const ListItems = items.map((item) => {
+    let title = item.url;
+    if (item.info && item.info.title) {
+      title = item.info.title;
+    }
+    return (
+      <li className={styles.item} key={item.id}>{title} {item.percent}</li>
+    );
+  });
 
   return (
     <ul className={styles.list}>
