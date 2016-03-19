1ff148e1addb6896495cf45fc15f8c646c2dc6b5
Emmanuel Pilande <epilande@gmail.com>
2016-03-18 18:13:23 -1000

    Display close icon in modal

--- app/components/Modal/Modal.js
+++ app/components/Modal/Modal.js
@@ -1,10 +1,16 @@
 import React, { PropTypes } from 'react';
+import Close from 'components/icons/X';
 import styles from './Modal.css';
 
 const Modal = ({ children }) => (
   <div className={styles.base}>
     <div className={styles.overlay}></div>
     <div className={styles.main}>
+      <Close
+        className={styles.close}
+        fill="#333"
+        size={20}
+      />
       {children}
     </div>
   </div>
