ccd3443e87ad6b94a5362113c8af496a7d1cdf5d
Emmanuel Pilande <epilande@gmail.com>
2016-03-19 22:52:41 -1000

    Restructure Dropdown component & add arrow

--- app/components/Dropdown/Dropdown.js
+++ app/components/Dropdown/Dropdown.js
@@ -2,18 +2,21 @@ import React, { PropTypes } from 'react';
 import styles from './Dropdown.css';
 
 const Dropdown = ({ className, name, items, value, selected, onChange }) => (
-  <select name={name}
-    className={`${className} ${styles.base}`}
-    value={value}
-    defaultValue={selected}
-    onChange={onChange}
-  >
-    {items.map((item, i) => (
-      <option key={i} value={item.value}>
-        {item.text}
-      </option>
-    ))}
-  </select>
+  <div className={styles.base}>
+    <select name={name}
+      className={className}
+      value={value}
+      defaultValue={selected}
+      onChange={onChange}
+    >
+      {items.map((item, i) => (
+        <option key={i} value={item.value}>
+          {item.text}
+        </option>
+      ))}
+    </select>
+    <span className={styles.arrow}></span>
+  </div>
 );
 
 Dropdown.defaultProps = {
