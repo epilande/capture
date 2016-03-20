54d710d88d1f3755b21771a9638c6e5e8eb67436
Emmanuel Pilande <epilande@gmail.com>
2016-03-19 20:05:10 -1000

    Allow default selected on dropdown

--- app/components/Dropdown/Dropdown.js
+++ app/components/Dropdown/Dropdown.js
@@ -1,14 +1,17 @@
 import React, { PropTypes } from 'react';
 import styles from './Dropdown.css';
 
-const Dropdown = ({ className, name, items, value, onChange }) => (
+const Dropdown = ({ className, name, items, value, selected, onChange }) => (
   <select name={name}
     className={`${className} ${styles.base}`}
     value={value}
+    defaultValue={selected}
     onChange={onChange}
   >
     {items.map((item, i) => (
-      <option key={i} value={item.value}>{item.text}</option>
+      <option key={i} value={item.value}>
+        {item.text}
+      </option>
     ))}
   </select>
 );
@@ -22,6 +25,7 @@ Dropdown.propTypes = {
   items: PropTypes.array,
   className: PropTypes.string,
   value: PropTypes.string,
+  selected: PropTypes.string,
   onChange: PropTypes.func,
 };
 
