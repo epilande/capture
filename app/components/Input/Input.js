54e9a9843d009bfece97564791150e082e3027fd
Emmanuel Pilande <manny.pilande@hpe.com>
2016-04-16 00:27:19 -1000

    Change Input to class component

--- app/components/Input/Input.js
+++ app/components/Input/Input.js
@@ -1,24 +1,47 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 
-const Input = ({ ...props }) => (
-  <input
-    {...props}
-  />
-);
+class Input extends Component {
+  static propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
+    type: PropTypes.string,
+    placeholder: PropTypes.string,
+    value: PropTypes.string,
+    disabled: PropTypes.bool,
+    onChange: PropTypes.func,
+    onKeyDown: PropTypes.func,
+  };
 
-Input.defaultProps = {
-  type: 'text',
-};
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onEnter = this.onEnter.bind(this);
 
-Input.propTypes = {
-  id: PropTypes.string,
-  className: PropTypes.string,
-  type: PropTypes.string,
-  placeholder: PropTypes.string,
-  value: PropTypes.string,
-  disabled: PropTypes.bool,
-  onChange: PropTypes.func,
-  onKeyDown: PropTypes.func,
-};
+    this.state = {
+      value: props.value || '',
+      valid: true,
+    };
+  }
+
+  onChange(event) {
+  }
+
+  onEnter(event) {
+  }
+
+  render() {
+    const { ...props } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div>
+        <input
+          value={value}
+          {...props}
+        />
+      </div>
+    );
+  }
+}
 
 export default Input;
